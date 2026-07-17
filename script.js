a7fa8e3ff197e4f391a4b82caf19efc688fa97a5
Asharib Nomani <asharibnomani2008@gmail.com>
2026-07-18 02:26:41 +0500

    Draw SVG path between selected cells

--- script.js
+++ script.js
@@ -104,5 +104,38 @@ function restartGame() {
 
 function redrawPath() {
 
-    // Next part :)
+    svg.innerHTML = "";
+
+    if (path.length < 2) return;
+
+    for (let i = 0; i < path.length - 1; i++) {
+
+        const first = path[i].getBoundingClientRect();
+        const second = path[i + 1].getBoundingClientRect();
+        const board = gameBoard.getBoundingClientRect();
+
+        const x1 = first.left + first.width / 2 - board.left;
+        const y1 = first.top + first.height / 2 - board.top;
+
+        const x2 = second.left + second.width / 2 - board.left;
+        const y2 = second.top + second.height / 2 - board.top;
+
+        const line = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "line"
+        );
+
+        line.setAttribute("x1", x1);
+        line.setAttribute("y1", y1);
+
+        line.setAttribute("x2", x2);
+        line.setAttribute("y2", y2);
+
+        line.setAttribute("stroke", "#0A66C2");
+        line.setAttribute("stroke-width", "8");
+        line.setAttribute("stroke-linecap", "round");
+
+        svg.appendChild(line);
+    }
+
 }
